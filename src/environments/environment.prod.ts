e4ed83abfeb4283c051617ddc634ee8de47800f0
tope <tope@winexviv.com>
2021-06-02 13:49:28 +0100

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
   production: true,
-  backendPath: 'https://tyckethub.com',
-  apiBaseUrl: 'https://tyckethub.com/api',
+  backendPath: 'https://admintyckethub.com',
+  apiBaseUrl: 'https://admintyckethub.com/api',
   apiExternalUrl: 'https://restcountries.eu/rest/v2/name/',
   httpOptions: {
     headers: new HttpHeaders({
