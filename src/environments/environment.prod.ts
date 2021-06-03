0be8dd3755a3197765ed3c789d02e9f7929131d8
tope <tope@winexviv.com>
2021-06-03 03:09:40 +0100

    included dist in push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
   production: true,
-  backendPath: 'https://admintyckethub.com',
-  apiBaseUrl: 'https://admintyckethub.com/api',
+  backendPath: 'https://admin.tyckethub.com',
+  apiBaseUrl: 'https://admin.tyckethub.com/api',
   apiExternalUrl: 'https://restcountries.eu/rest/v2/name/',
   httpOptions: {
     headers: new HttpHeaders({
